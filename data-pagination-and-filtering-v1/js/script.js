29778d90515987fdfbcc53cecaff931f2b07a5dd
Marina Harrison <marinaharrison1@gmail.com>
2021-03-18 10:55:26 -0700

    Wrote the showPage function

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -4,7 +4,6 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 
-
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
@@ -17,7 +16,35 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const list = (data.js);
+const perPage = 9;
 
+function showPage (list, page) {
+   let startIndex = (page * 9) - 9;
+   let endIndex = page * 9;
+
+   let studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+    for (let i = 0; i < list.length; i++) {
+       if(i >= startIndex && i <= endIndex) {
+          let studentIndex = list[i];
+          let studentItem = `<li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+            <h3>Ethel Dean</h3>
+            <span class="email">ethel.dean@example.com</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined 12-15-2005</span>
+          </div>
+        </li>`;
+
+         studentList.insertAdjacentHTML("beforeend", studentItem);
+
+console.log('studentIndex');
+       }
+    }
+}
 
 
 /*
